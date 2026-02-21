382b3038d58a0c0989709efc2c875fdf25408312
w7rpp <bj0619@srmist.edu.in>
2026-02-21 16:50:35 +0530

    [Brendan] added: Created Java Program to Convert Kilometers to Miles

---
+++ Level1/DistanceConversion.java
@@ -0,0 +1,8 @@
+public class DistanceConversion{
+	public static void main(String[] args){
+		double KM_inp = 10.8;
+		double KM_mile = 1.6;
+		double KM_op = KM_inp * KM_mile;
+		System.out.println("The distance " +KM_inp+ "km in miles is " +KM_op);
+	}
+}
