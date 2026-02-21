07904203dbe097b99768511e16ffc6f32482c209
w7rpp <bj0619@srmist.edu.in>
2026-02-21 16:39:12 +0530

    [Brendan] added: Created Java Program to Calculate Average Percentage of given marks

---
+++ Level1/AverageMark.java
@@ -0,0 +1,9 @@
+public class AverageMark{
+	public static void main(String[] args){
+		int MathMark = 94;
+		int PhyMark = 95;
+		int ChemMark = 96;
+		double AvgMark = (MathMark + PhyMark + ChemMark)/3.0;
+		System.out.println("Sam's Average Mark in PCM is: " +AvgMark);
+	}
+}
