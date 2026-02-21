cbf3948ad0ea2c6d23d3670eec9f77ceae06dd08
w7rpp <bj0619@srmist.edu.in>
2026-02-21 16:27:08 +0530

    [Brendan] added: Created Java Program to Calculate Age.

---
+++ Level1/AgeCalc.java
@@ -0,0 +1,8 @@
+public class AgeCalc{
+	public static void main(String[] args){
+		int BirthYear = 2000;
+		int CurrentYear = 2024;
+		int age = CurrentYear - BirthYear;
+		System.out.println("Harry's Age in 2024 is:" +age);
+	}
+}
